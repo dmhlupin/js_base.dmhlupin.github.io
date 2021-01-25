b4b93c2e87e43afd777beced91c8fd313a3e7b0b
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-01-25 17:07:11 +0300

    lesson 2 tasks

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-"use strict"
+//
 
-// Первое задание
-const tempC = 20;
+const button1 = document.getElementById("button1");
+button1.onclick = calcTwoDigits;
+resultElement = document.getElementById("result1");
 
-const tempF = (9/5)*tempC + 32;
+function calcTwoDigits() {
+    const a = Number(document.getElementById("digit1").value);
+    const b = Number(document.getElementById("digit2").value);
+    let result;
+    let resString;
+    if (a >= 0 && b >= 0) {
+        result = a - b;
+        resString = "a и b положительны. Вычитаем: a - b = "+ result;
+    } else if (a <= 0 && b <= 0) {
+        result = a * b;
+        resString = "a и b отрицательны. Перемножаем: a * b = "+ result;
+    } else {
+        result = a + b;
+        resString = "a и b разных знаков. Складываем: a + b = "+ result;
+    }
+    resultElement.innerHTML = "итоговый результат: " + resString;
+    console.log(result);
+}
 
-alert(`Задание 1: ${tempC} градусов Цельсия - это ${tempF} градусов по Фаренгейту`);
-
-// Второе задание
-
-
-let admin;
-let name = "Василий";
-
-admin = name;
-alert(`Задание 2: ${admin}`);
-
-// третье задание
-
-const concat = 1000 + "108";
-
-alert(`Задание 3: 1000 + "108" = ${concat} так как при сложении строки и числа происходит конкатенация. Тип результата = ${typeof(concat)}`);
-
-// Четвертое задание
-
-alert("Задание 4: атрибут async загружает скрипт асинхронно, т.е. его загрузка не зависит от загрузки страницы или других скриптов и наоборот.");
-alert("Задание 4: атрибут defer позволяет браузеру загружать скрипт в фоновом режиме, и запускать его когда он загрузится. Не блокирует загрузку страницы.");
