6e0b923352a66b6c19c452fad7d5b952ab87d8c7
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-02-16 19:37:49 +0300

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,50 +1,68 @@
 class Cart {
-    constructor (block) {
+    constructor(block) {
         this.block = block;
     }
     items = {};
     countOfItems = 0;
     fullPrice = 0;
+    cartBlocksArray = [];
+    address = {
+        city: "",
+        street: "",
+        postal: ""
+    };
+    comments = "";
+    
+    
 
     addToCart(item) {
         const quantityOfItems = products.items[item.id].quantity;
-        if(item.id in this.items) {
-            if(this.items[item.id].quantity < quantityOfItems) {
+        if (item.id in this.items) {
+            if (this.items[item.id].quantity < quantityOfItems) {
                 this.items[item.id].quantity += 1;
             }
-        }
-        else {
-            this.items[item.id] = {item: item, quantity: 1}
-            
+        } else {
+            this.items[item.id] = {
+                item: item,
+                quantity: 1
+            }
+
         }
         this.totalPrice();
-        this.showCart();
+        this.showCart(this.createCartBlock());
     }
 
-    showCart() {
-        console.log(this.items);
-        this.block.innerHTML = "Корзина";
+
+
+    showCart(changedBlock = "Корзина пуста!") {
+        this.block.innerHTML = "";
+        this.block.append(changedBlock);
+    };
+
+    createCartBlock() {
+        const blockTitle = document.createElement("p");
+        blockTitle.innerHTML = "Корзина";
         const total = document.createElement("div");
         total.classList.add("totalCart");
         const container = document.createElement("div");
         container.classList.add("cartContainer");
-        for(let i in this.items) {
+        for (let i in this.items) {
             const currentItem = this.items[i];
             const itemDiv = document.createElement("div");
             const sliderDiv = document.createElement("div");
             const descriptionDiv = document.createElement("div");
-            
+
 
             const headItem = document.createElement("h3");
             const priceItem = document.createElement("p");
             const quantityItem = document.createElement("p");
             const removeButton = document.createElement("div");
-            
+
             itemDiv.classList.add("itemCartList");
             sliderDiv.classList.add("sliderDiv");
             descriptionDiv.classList.add("cartDescDiv");
             removeButton.classList.add("removeButton");
-            
+
             headItem.classList.add("headItem");
             priceItem.classList.add("priceItem");
             quantityItem.classList.add("quantityItem");
@@ -57,17 +75,88 @@ class Cart {
             headItem.innerHTML = currentItem.item.name;
             priceItem.innerHTML = `Цена: ${currentItem.item.price} рублей.`;
             quantityItem.innerHTML = `В корзине ${currentItem.quantity} шт.`;
-            
-            const cartItemSlider = new Slider(sliderDiv,currentItem.item.images);
-            descriptionDiv.append(headItem, priceItem,quantityItem,removeButton);
-            itemDiv.append(sliderDiv,descriptionDiv);
+
+            const cartItemSlider = new Slider(sliderDiv, currentItem.item.images);
+            descriptionDiv.append(headItem, priceItem, quantityItem, removeButton);
+            itemDiv.append(sliderDiv, descriptionDiv);
             container.append(itemDiv);
         }
-        total.innerHTML = `В вашей корзине ${this.countOfItems} товаров на ${this.fullPrice} рублей.`
-        this.block.append(total, container);
+        total.innerHTML = `В вашей корзине ${this.countOfItems} товаров на ${this.fullPrice} рублей.`;
+        const nextButton = document.createElement("div");
+        nextButton.innerHTML = 'Далее';
+        nextButton.classList.add("nextButton");
+        nextButton.addEventListener("click", () => {
+            this.showCart(this.createAddressBlock());
+        });
+        const block = document.createElement("div");
+        block.append(blockTitle, total, container, nextButton);
+        console.log(block);
+        return block;
+
+
+
+    }
+
+    createAddressBlock() {
+        const addressBlock = document.createElement("div");
+        const blockTitle = document.createElement("p");
+        const container = document.createElement("div");
+        const nextButton = document.createElement("div");
+        blockTitle.innerHTML = "Заполните форму доставки:"
+        nextButton.innerHTML = 'Далее';
+        const cityInput = document.createElement('input');
+        const streetInput = document.createElement('input');
+        const postalCode = document.createElement('input');
+        if (this.address.city === "") {
+            cityInput.placeholder = "Введите ваш город";
+        } else cityInput.value = this.address.city;
+        if (this.address.street === "") {
+            streetInput.placeholder = "Введите вашу улицу";
+        } else streetInput.value = this.address.street;
+        if (this.address.postal === "") {
+            postalCode.placeholder = "Введите ваш POST code";
+        } else postalCode.value = this.address.postal;
         
+        container.append(cityInput, streetInput, postalCode);
+        container.classList.add("addressBlock");
+        nextButton.classList.add("nextButton");
+        nextButton.addEventListener("click", () => {
+            this.address = {
+                city: cityInput.value,
+                street: cityInput.value,
+                postal: postalCode.value
+            };
+            this.showCart(this.createCommentsBlock());
+        });
+        const block = document.createElement("div");
+        addressBlock.append(blockTitle, container, nextButton);
+        console.log(block);
 
+        return addressBlock;
+    }
 
+    createCommentsBlock() {
+        const commentsBlock = document.createElement("div");
+        const blockTitle = document.createElement("p");
+        const container = document.createElement("div");
+        const nextButton = document.createElement("div");
+        blockTitle.innerHTML = "Добавьте комментарии:"
+        nextButton.innerHTML = 'Готово';
+        const commentInput = document.createElement('textarea');
+        if (this.comments === "") {
+            commentInput.placeholder = "Вводите коментарий сюда";
+        } else {
+            commentInput.value = this.comments
+        };
+        container.classList.add("commentsBlock");
+        nextButton.classList.add("nextButton");
+        nextButton.addEventListener("click", () => {
+            this.comments = commentInput.value;
+            this.showCart("Готово!")
+        })
+        container.append(commentInput);
+        commentsBlock.append(blockTitle, container, nextButton);
+        return commentsBlock;
     }
 
     totalPrice() {
@@ -79,12 +168,14 @@ class Cart {
             this.fullPrice += thisGood.item.price * thisGood.quantity;
             this.countOfItems += thisGood.quantity;
         }
-        
+
     }
     removeItems(itemId) {
         delete this.items[itemId];
         this.totalPrice();
-        this.showCart();
+        if (!this.fullPrice) this.showCart()
+        else {
+            this.showCart(this.createCartBlock());
+        }
     }
 }
-
--- main.js
+++ main.js
@@ -5,12 +5,14 @@ document.head.insertAdjacentHTML("afterbegin", '<link rel="stylesheet" href="htt
 
 const phone = new Product("0001", "Phone", 500, ["./img/0001_1.jpg","./img/0001_2.jpg","./img/0001_3.jpg","./img/0001_4.jpg"]);
 const laptop = new Product("0002", "Laptop", 1500, ["./img/0002_1.jpg","./img/0002_2.jpg","./img/0002_3.jpg"]);
-const pc = new Product("0003", "PC", 2000, ["./img/0003_1.jpg","./img/0003_2.jpg",]);
+const pc = new Product("0003", "PC", 2000, ["./img/0003_1.jpg","./img/0003_2.jpg"]);
+const tab = new Product("0004", "Tab", 1300, ["./img/0004_1.jpg","./img/0004_2.jpg","./img/0004_3.jpg"]);
 
 
 
 const productBlock = document.getElementById("productBlock");
 const cartBlock = document.getElementById("cartBlock");
+
 
 const products = new Products(productBlock);
 const cart = new Cart(cartBlock);
@@ -18,6 +20,8 @@ const cart = new Cart(cartBlock);
 products.addItem(phone, 3);
 products.addItem(laptop,4);
 products.addItem(pc,6);
+products.addItem(tab,5);
 products.showItems();
 cart.showCart();
+
 
